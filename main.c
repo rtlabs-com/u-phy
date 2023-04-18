db0196100775f6bb9dadb5bf9dcfb0c126e6feb9
Ola Björsne <ola.bjorsne@rt-labs.com>
2023-04-18 16:49:57 +0200

    refactor mono application
    Add autostart of configuration.
    Run application as a task to allow continued
    use of the shell.
    
    main.c is a bit complex with a lot of configuration options.
    
    Change-Id: I1c8c7ec9d6a1697005436405fedb7d360a95578c

--- main.c
+++ main.c
@@ -29,7 +29,12 @@
 #include "bsp.h"
 #include "gpio.h"
 #include "shell.h"
-#endif
+
+#define STORAGE_ROOT "/disk1" /* No trailing slash */
+#define APP_TASK_PRIO       5
+#define APP_TASK_STACK_SIZE 6000
+
+#endif /* defined(__rtk__) */
 
 #if defined(__linux__)
 #define ENABLE_IO_FILES
@@ -53,53 +58,15 @@ extern const uint8_t _eeprom_bin_end;
 static void cb_avail (up_t * up, void * user_arg)
 {
    up_read_outputs (up);
-
-   /* Process outputs */
-#if 0
-   up_data.io8.o8 = my_filter (up_data.io8.o8);
-#endif
 }
 
 static void cb_sync (up_t * up, void * user_arg)
 {
-#if 0
-   /* Activate outputs */
-   gpio_set (GPIO_LED_D6, up_data.io8.o8 & BIT (0));
-   gpio_set (GPIO_LED_D7, up_data.io8.o8 & BIT (1));
-   gpio_set (GPIO_LED_D8, up_data.io8.o8 & BIT (2));
-   gpio_set (GPIO_LED_D9, up_data.io8.o8 & BIT (3));
-
-   /* Latch inputs (read back leds for now) */
-   up_data.i8.i8 =
-      gpio_get (GPIO_LED_D6) << 0 |
-      gpio_get (GPIO_LED_D7) << 1 |
-      gpio_get (GPIO_LED_D8) << 2 |
-      gpio_get (GPIO_LED_D9) << 3;
-   up_data.io8.i8 =
-      gpio_get (GPIO_LED_D6) << 0 |
-      gpio_get (GPIO_LED_D7) << 1 |
-      gpio_get (GPIO_LED_D8) << 2 |
-      gpio_get (GPIO_LED_D9) << 3;
-
-   /* Process inputs */
-   /* up_data.io8.i8 = my_filter (up_data.io8.i8); */
-
-#if 0
-   /* Simulate inputs */
-   double t = tick_get();
-   up_data.i8.i8 = 100 + 100 * sin (t/1000.0);
-   up_data.io8.i8 = 80 + 80 * cos (t/800.0);
-#endif
-#endif
-
    up_write_inputs (up);
 
 #ifdef ENABLE_IO_FILES
    up_util_read_input_file ("/tmp/u-phy-input.txt");
    up_util_write_status_file ("/tmp/u-phy-status.txt");
-#else
-   up_data.I8.Input_8_bits.value++;
-   up_data.I8O8.Input_8_bits.value++;
 #endif
 }
 
@@ -123,6 +90,39 @@ static void cb_param_write_ind (up_t * up, void * user_arg)
 static void cb_status_ind (up_t * up, uint32_t status, void * user_arg)
 {
    /* Ignore */
+}
+
+static int str_to_bus_config (const char * str, up_bustype_t * bustype)
+{
+   if (str == NULL || bustype == NULL)
+   {
+      return -1;
+   }
+
+#ifdef UP_DEVICE_PROFINET_SUPPORTED
+   if (strcmp (str, "profinet") == 0)
+   {
+      *bustype = UP_BUSTYPE_PROFINET;
+      return 0;
+   }
+#endif
+
+#ifdef UP_DEVICE_ETHERCAT_SUPPORTED
+   if (strcmp (str, "ethercat") == 0)
+   {
+      *bustype = UP_BUSTYPE_ECAT;
+      return 0;
+   }
+#endif
+
+   if (strcmp (str, "mock") == 0)
+   {
+      *bustype = UP_BUSTYPE_MOCK;
+      return 0;
+   }
+
+   printf ("Unsupported fieldbus \"%s\". Is it supported by device model?\n", str);
+   return -1;
 }
 
 static const char * error_code_to_str (up_error_t error_code)
@@ -168,7 +168,50 @@ static void cb_loop_ind (up_t * up, void * user_arg)
    up_util_read_input_file ("/tmp/u-phy-input.txt");
    up_util_write_status_file ("/tmp/u-phy-status.txt");
 }
+
+/**
+ * Initialize utility files.
+ * - Generate template input file and initialize up data
+ *  - Generate default status file
+ */
+static void init_util_files (void)
+{
+   up_util_write_input_file ("/tmp/u-phy-input.txt");
+   up_util_read_input_file ("/tmp/u-phy-input.txt");
+   up_util_write_status_file ("/tmp/u-phy-status.txt");
+}
 #endif
+
+#if !defined(OPTION_MONO)
+static int init_rpc_transport (up_t * up, const char * cfg)
+{
+#if defined(OPTION_TRANSPORT_TCP)
+   if (up_tcp_transport_init (up, cfg, 5150) != 0)
+   {
+      printf ("Failed to bring up transport\n");
+      return -1;
+   }
+#endif
+
+#if defined(__rtk__) && defined(OPTION_TRANSPORT_UART)
+   if (up_uart_transport_init (up, cfg) != 0)
+   {
+      printf ("Failed to bring up transport\n");
+      return -1;
+   }
+#endif
+
+#if defined(__linux__) && defined(OPTION_TRANSPORT_UART)
+   if (up_serial_transport_init (up, cfg) != 0)
+   {
+      printf ("Failed to bring up transport\n");
+      return -1;
+   }
+#endif
+
+   return 0;
+}
+#endif /* !defined(OPTION_MONO) */
 
 static up_busconf_t up_busconf;
 
@@ -187,111 +230,23 @@ static up_cfg_t cfg = {
 #endif
 };
 
-int _cmd_start (int argc, char * argv[])
+void up_app_main (void * arg)
 {
-   int error;
-#ifdef ENABLE_IO_FILES
+   up_t * up = (up_t *)arg;
+#if defined(ENABLE_IO_FILES)
    bool first_run = true;
 #endif
 
-   /* Check command line arguments */
-   if (argc != 3)
-   {
-      printf ("usage: %s <transport cfg> <fieldbus>\n", argv[0]);
-      printf ("The <fieldbus> can be profinet, ethercat or mock\n");
-      printf ("example: %s /dev/ttyUSB0 profinet\n", argv[0]);
-      return -1;
-   }
-
-#ifdef OPTION_MONO
-   extern int core_main (void);
-   core_main();
-#endif
-
-   if (strcmp (argv[2], "profinet") == 0)
-   {
-#ifdef UP_DEVICE_PROFINET_SUPPORTED
-      cfg.device->bustype = UP_BUSTYPE_PROFINET,
-      up_busconf.profinet = up_profinet_config;
-#else
-      printf ("The device model has no Profinet configuration\n");
-      return -1;
-#endif
-   }
-   else if (strcmp (argv[2], "ethercat") == 0)
-   {
-#ifdef UP_DEVICE_ETHERCAT_SUPPORTED
-      cfg.device->bustype = UP_BUSTYPE_ECAT,
-      up_busconf.ecat = up_ethercat_config;
-#else
-      printf ("The device model has no EtherCAT configuration\n");
-      return -1;
-#endif
-   }
-   else if (strcmp (argv[2], "mock") == 0)
-   {
-      cfg.device->bustype = UP_BUSTYPE_MOCK;
-      up_busconf.mock = up_mock_config;
-   }
-   else
-   {
-      printf ("Unsupported fieldbus \"%s\", abort\n", argv[2]);
-      return -1;
-   }
-
-   /* Initialise U-Phy and set up transport */
-
-   printf ("Starting sample host application\n");
-   up_t * up = up_init (&cfg);
-
-#if !defined(OPTION_MONO)
-#if defined(OPTION_TRANSPORT_TCP)
-   error = up_tcp_transport_init (up, argv[1], 5150);
-   if (error)
-   {
-      printf ("Failed to bring up transport\n");
-      exit (EXIT_FAILURE);
-   }
-#endif
-
-#if defined(__rtk__) && defined(OPTION_TRANSPORT_UART)
-   error = up_uart_transport_init (up, argv[1]);
-   if (error)
-   {
-      printf ("Failed to bring up transport\n");
-      exit (EXIT_FAILURE);
-   }
-#endif
-
-#if defined(__linux__) && defined(OPTION_TRANSPORT_UART)
-   error = up_serial_transport_init (up, argv[1]);
-   if (error)
-   {
-      printf ("Failed to bring up transport\n");
-      exit (EXIT_FAILURE);
-   }
-#endif
-
-   error = up_rpc_init (up);
-   if (error)
-   {
-      printf ("Failed to init rpc transport\n");
-      exit (EXIT_FAILURE);
-   }
-#endif /* !defined(OPTION_MONO) */
+   /* Delay startup so that there is time to restore the second serial
+    * connection, used for logging, before the device configuration is
+    * sent to the core.
+    */
+   os_usleep (2000 * 1000);
 
    while (true)
    {
-
-      /* Delay startup so that there is time to restore the second serial
-       * connection, used for logging, before the device configuration is
-       * sent to the core.
-       */
-      os_usleep (2000 * 1000);
-
 #if !defined(OPTION_MONO)
-      error = up_rpc_start (up, true);
-      if (error)
+      if (up_rpc_start (up, true) != 0)
       {
          printf ("Failed to connect to u-phy core\n");
          exit (EXIT_FAILURE);
@@ -306,15 +261,14 @@ int _cmd_start (int argc, char * argv[])
 
       if (cfg.device->bustype == UP_BUSTYPE_ECAT)
       {
-         error = up_write_ecat_eeprom (
-            up,
-            &_eeprom_bin_start,
-            &_eeprom_bin_end - &_eeprom_bin_start);
-
-         if (error != 0)
+         if (
+            up_write_ecat_eeprom (
+               up,
+               &_eeprom_bin_start,
+               &_eeprom_bin_end - &_eeprom_bin_start) != 0)
          {
             printf ("Failed to write EtherCAT eeprom \n");
-            return error;
+            return;
          }
       }
 
@@ -324,17 +278,11 @@ int _cmd_start (int argc, char * argv[])
          exit (EXIT_FAILURE);
       }
 
-#ifdef ENABLE_IO_FILES
+#if defined(ENABLE_IO_FILES)
       if (first_run)
       {
          first_run = false;
-
-         /* Generate template input file and initialize up data */
-         up_util_write_input_file ("/tmp/u-phy-input.txt");
-         up_util_read_input_file ("/tmp/u-phy-input.txt");
-
-         /* Generate default status file */
-         up_util_write_status_file ("/tmp/u-phy-status.txt");
+         init_util_files();
       }
 #endif
 
@@ -362,6 +310,95 @@ int _cmd_start (int argc, char * argv[])
       printf ("Restart application\n");
       printf ("Reset Core and reconfigure device\n");
    }
+}
+
+int _cmd_start (int argc, char * argv[])
+{
+   up_t * up;
+#if !defined(OPTION_MONO)
+   char * transport = NULL;
+#endif
+   char * fieldbus;
+
+   /* Check command line arguments */
+#if defined(OPTION_MONO)
+   if (argc != 2)
+   {
+      printf ("error - try \"help %s\n", argv[0]);
+      return -1;
+   }
+   fieldbus = argv[1];
+#else  /* defined(OPTION_MONO) */
+   if (argc != 3)
+   {
+      printf ("usage: %s <transport cfg> <fieldbus>\n", argv[0]);
+      printf ("The <fieldbus> can be profinet, ethercat or mock\n");
+      printf ("example: %s /dev/ttyUSB0 profinet\n", argv[0]);
+      return -1;
+   }
+   transport = argv[1];
+   fieldbus = argv[2];
+#endif /* defined(OPTION_MONO) */
+
+   /* Initialise U-Phy and set up transport */
+
+#ifdef OPTION_MONO
+   extern int up_core_main (void);
+   if (up_core_main() != 0)
+   {
+      printf ("Failed to start U-Phy core\n");
+   }
+#endif
+
+   if (str_to_bus_config (fieldbus, &cfg.device->bustype) == 0)
+   {
+      switch (cfg.device->bustype)
+      {
+      case UP_BUSTYPE_PROFINET:
+         up_busconf.profinet = up_profinet_config;
+         break;
+      case UP_BUSTYPE_ECAT:
+         up_busconf.ecat = up_ethercat_config;
+         break;
+      case UP_BUSTYPE_MOCK:
+         up_busconf.mock = up_mock_config;
+      }
+   }
+   else
+   {
+      printf ("Unsupported fieldbus \"%s\", abort\n", argv[2]);
+      return -1;
+   }
+
+   printf ("Starting sample application\n");
+   up = up_init (&cfg);
+
+#if defined(OPTION_MONO)
+
+   os_thread_create (
+      "up_app_main",
+      APP_TASK_PRIO,
+      APP_TASK_STACK_SIZE,
+      up_app_main,
+      up);
+
+#else /* defined(OPTION_MONO) */
+
+   if (init_rpc_transport (up, transport) != 0)
+   {
+      printf ("Failed to init rpc transport\n");
+      exit (EXIT_FAILURE);
+   }
+
+   if (up_rpc_init (up) != 0)
+   {
+      printf ("Failed to init rpc\n");
+      exit (EXIT_FAILURE);
+   }
+
+   up_app_main (up);
+
+#endif /* defined(OPTION_MONO) */
 
    return 0;
 }
@@ -370,15 +407,101 @@ int _cmd_start (int argc, char * argv[])
 
 const shell_cmd_t cmd_start = {
    .cmd = _cmd_start,
-   .name = "start",
-   .help_short = "start uphy",
-   .help_long = ""};
+   .name = "up_start",
+   .help_short = "start u-phy device",
+   .help_long =
+#if defined(OPTION_MONO)
+      "Start monolithic u-phy device including core and device model.\n"
+      "Usage: up_start <fieldbus>\n"
+      "where fieldbus can be ethercat, profinet or mock\n"};
+#else
+      "Start u-phy host device.\n"
+      "Usage: up_start <transport> <fieldbus>\n"
+      "where fieldbus can be ethercat, profinet or mock\n"};
+#endif
 
 SHELL_CMD (cmd_start);
 
+#if defined(OPTION_MONO)
+int _cmd_autostart (int argc, char * argv[])
+{
+   up_bustype_t bustype;
+   char * fieldbus;
+
+   if (argc != 2)
+   {
+      printf ("error - try \"help %s\n", argv[0]);
+      return -1;
+   }
+
+   fieldbus = argv[1];
+   if ((argc == 2) && (str_to_bus_config (fieldbus, &bustype) == 0))
+   {
+      int f = open (STORAGE_ROOT "/autostart", O_WRONLY | O_CREAT);
+      write (f, fieldbus, strlen (fieldbus) + 1);
+      close (f);
+
+      printf ("%s autostart added\n", fieldbus);
+   }
+   else
+   {
+      printf ("autostart disabled\n");
+
+      unlink (STORAGE_ROOT "/autostart");
+      return -1;
+   }
+
+   return 0;
+}
+
+const shell_cmd_t cmd_autostart = {
+   .cmd = _cmd_autostart,
+   .name = "up_autostart",
+   .help_short = "configure u-phy device autostart",
+   .help_long = "Set u-phy autostart configuration \n"
+                "Usage: up_autostart <fieldbus>\n"
+                "where fieldbus can be ethercat | profinet | mock.\n"
+                "If no valid fieldbus is given, autostart is disabled."};
+
+SHELL_CMD (cmd_autostart);
+
+/**
+ * Read auto start configuration file and start the
+ * device configuration by calling the start command.
+ *
+ * @return 0 if a device configuration was started.
+ *         -1 if no device configuration exists or on error
+ */
+int auto_start (void)
+{
+   up_bustype_t bustype;
+   char buf[32];
+   int f = open (STORAGE_ROOT "/autostart", O_RDONLY);
+   if (f > 0)
+   {
+      read (f, buf, sizeof (buf));
+      close (f);
+
+      if (str_to_bus_config (buf, &bustype) == 0)
+      {
+         char * argv[2] = {"up_start", buf};
+         return _cmd_start (2, argv);
+      }
+   }
+   return -1;
+}
+
+#endif /* defined(OPTION_MONO) */
+
 int main (int argc, char * argv[])
 {
-   return 0;
+#if defined(OPTION_MONO)
+   if (auto_start() != 0)
+   {
+      extern void core_reconfigure_net_if (void);
+      core_reconfigure_net_if();
+   }
+#endif
 }
 
 #else
